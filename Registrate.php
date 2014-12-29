277eb0ee5be18a35e3f9bef8d87d6feb7d161d08
alexskull <domingoskull3@gmail.com>
2014-12-28 19:40:55 -0430

    Registro de Usuario

---
+++ Registrate.php
@@ -0,0 +1,276 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Registrate</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<style type="text/css">
+<!--
+-->
+</style>
+<noscript>
+<link rel="stylesheet" href="css/5grid/core.css" />
+<link rel="stylesheet" href="css/5grid/core-desktop.css" />
+<link rel="stylesheet" href="css/5grid/core-1200px.css" />
+<link rel="stylesheet" href="css/5grid/core-noscript.css" />
+<link rel="stylesheet" href="css/style.css" />
+<link rel="stylesheet" href="css/style-desktop.css" />
+<link rel="stylesheet" href="css/least.min.css" />
+</noscript>
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery.lazyload.min.js"></script>
+<script src="js/least.min.js?03072013"></script>
+<script src="js/prism.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script src="css/5grid/init.js?use=mobile,desktop,1000px&amp;mobileUI=1&amp;mobileUI.theme=none&amp;mobileUI.openerWidth=52"></script>
+</head><body class="twocolumn2">
+<div id="wrapper">
+	<div id="header-wrapper">
+		<header id="header">
+			<div class="5grid-layout">
+				<div class="row">
+					<div class="12u" id="logo"> <!-- Logo -->
+						<h1>&nbsp;</h1>
+						<p>&nbsp;</p>
+						<p>&nbsp;</p>
+						<p>&nbsp;</p>
+						<p>&nbsp;</p>
+						<p>&nbsp;</p>
+						<p>&nbsp;</p>
+				  </div>
+				</div>
+			</div>
+			<div class="5grid-layout">
+				<div class="row">
+					<div class="12u" id="menu">
+						<div id="menu-wrapper">
+							<nav class="mobileUI-site-nav">
+								<ul>
+									<li><a href="index.php">Inicio</a></li>
+									<li><a href="Nosotros.html">Nosotros</a></li>
+									<li><a href="Destinos.html">Destinos</a></li>
+									<li><a href="Galeria.html">Galeria</a></li>
+									<li><a href="Contactanos.html">Contactanos</a></li>
+                                    <li class="current_page_item"><a href="Registrate.html">Registrate</a></li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+	</div>
+	<div id="page-wrapper" class="5grid-layout">
+		<div class="row">
+			<div class="3u">
+				<div id="sidebar2">
+					<section>
+						<br><br><br><br><br><br>
+
+						<a href="#"><img src="images/FF01.jpg" alt="" class="img-alignleft"></a>	
+							
+						
+						<div class="sbox2">
+							
+						</div>
+					</section>
+				</div>
+			</div>
+			<div class="9u mobileUI-main-content">
+				<div id="content">
+					<section>
+						<div class="post" align="left">
+							
+							<h2 align="right">Registro de Usuario</h2>
+						  	<section>
+                           	  <form method="POST" name="registrodeusa" >
+                           	      	<h12>
+                                    <p>
+                           	      	 	 <span class="Estilo2">Cedula(*)</span>
+                           	      	 	<input name="Cedula" type="number" class="laR"  maxlength="9" required title="INGRESANDO LA CEDULA SIN CARACTERES ESPECIALES"  placeholder="Ej:10998998" >
+               	      	    		</p>
+                                    <p>
+                                    	<span class="Estilo1 Estilo2">Nombres (*)</span>
+                           	      	  	<input name="Nombre" type="text" class="laR"  maxlength="25" required title="INGRESANDO EL NOMBRE"  placeholder="Ej:juan carlos">
+                   	      	    	</p>
+                                    <p>
+                                    	 <span class="Estilo1 Estilo2">Apellidos (*)</span>
+                           	      	  	<input name="Apellido" type="text" class="laR"  maxlength="25" required title="INGRESANDO LOS APELLIDOS"  placeholder="Ej:perez perez">
+                   	      	    	</p>
+	                       	      	<p>
+	                                    <span class="Estilo1 Estilo2">Telefono Local (*)</span>
+	                   	      	  	  	<input name="THabitacion" type="number" class="laR"  maxlength="14"  required title="INGRESANDO EL TELEFONO LOCAL CON CODIGO DE AREA"  placeholder="Ej:02763434455">
+	               	      	    	</p>
+                                    
+                                     <p>
+                                       <span class="Estilo1 Estilo2">Telefono Movil</span>
+                                       <input name="TCelular" type="number" class="laR"  maxlength="14"  title="INGRESANDO EL TELEFONO MOVIL CON "  placeholder="Ej:04247766555">
+                                      </p>
+                                     <p>
+                                     	<span class="Estilo1 Estilo2">Fecha de Nacimiento (*)</span>
+                           	      	  	<input name="Fnacimiento" type="date" class="laR" required >
+                   	      	    	</p>
+                   	      	    	<p>
+                                    	<span class="Estilo1 Estilo2">Estado (*)</span>
+                           	      	  	<input name="estado" type="text" class="laR"  maxlength="25" required title="INGRESANDO EL ESTADO"  placeholder="Ej:Tachira">
+                   	      	    	</p>
+                   	      	    	<p>
+                                    	<span class="Estilo1 Estilo2">Ciudad (*)</span>
+                           	      	  	<input name="ciudad" type="text" class="laR"  maxlength="25" required title="INGRESANDO LA CIUDAD"  placeholder="Ej:Sna Cristobal">
+                   	      	    	</p>
+                   	      	    	<p>
+                                    	<span class="Estilo1 Estilo2">Sector (*)</span>
+                           	      	  	<input name="sector" type="text" class="laR"  maxlength="25" required title="INGRESANDO EL SECTOR DONDE VIVE"  placeholder="Ej:Paramillo">
+                   	      	    	</p>
+                   	      	    	<p>
+                                     	<span class="Estilo1 Estilo2">E-Mail(*)</span>
+                           	      	  	<input name="Email" type="email" class="laR" required pattern="[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}" title="INGRESANDO EL TELEFONO LOCAL CON CODIGO DE AREA"  placeholder="Ej:juan@empresa.com">
+                   	      	    	</p>
+                                     <p>
+                                     	<span class="Estilo1 Estilo2">Contraseña (*)</span>
+                           	      	  	<input name="Contraseña1" type="password" class="laR"  required title="INGRESANDO LA CONTRASEÑA" placeholder="Debe ser mayor de 6 caracteres">
+                   	      	    	</p>
+                                     <p>
+                                     	<span class="Estilo1 Estilo2">Confirmación de Contraseña (*)</span>
+                           	      	  	<input name="Contraseña2" type="password" class="laR"  required title="INGRESANDO LA CONTRASEÑA" placeholder="Debe ser mayor de 6 caracteres">
+                   	      	    	</p>
+                   	      	    	<?php 
+                   	      	    	?>
+                                     <p>
+                       	      	      <input  name="Regis" value="REGISTRAR" type="submit"  class="input-botonC"  >
+                           	      	  
+                   	      	          </p> </h12>
+                           	  </form>	
+                            </section>
+						</div>
+					</section>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div id="copyright" class="5grid-layout">
+	<section>
+		<p>&copy;2014 Oramaika   | Diseño:  Ing. Software UNET</p>
+        <p><a href="https://www.facebook.com/embarcadero.chichirivicheedofalcon">FACEBOOK: Embarcadero Oramaika</a></p>
+	</section>
+</div>
+</body>
+<?php 
+if (isset($_POST['Regis']))
+{
+	$password=$_POST['Contraseña1'];
+	if (strlen($password) < 6) 
+	{
+		$ban=false;
+		echo "<script type='text/javascript'>alert('AVISO: La contraseña es demasiado corta. Por favor, introduzca al menos 6 caracteres');</script>";  
+	} 
+	else
+	{
+		if ( $password != $_POST["Contraseña2"]) 
+		{
+			echo "<script type='text/javascript'>alert('AVISO: Las contraseñas no coinciden. Por favor, inténtelo de nuevo');</script>";
+			$ban=false;
+		}
+		else
+		{
+			$ban=true;
+		}
+	}
+	if ($ban==true) 
+	{
+	 	$link = mysql_connect('localhost', 'root')
+	    or die('No se pudo conectar: ' . mysql_error());
+		mysql_select_db('oramaika') or die('No se pudo seleccionar la base de datos');
+		//variables String
+        $nombre=mysql_real_escape_string($_POST['Nombre']);
+        $apellido=mysql_real_escape_string($_POST['Apellido']);
+        $estado=mysql_real_escape_string($_POST['estado']);
+        $ciudad=mysql_real_escape_string($_POST['ciudad']);
+        $sector=mysql_real_escape_string($_POST['sector']);
+        $email=mysql_real_escape_string($_POST['Email']);
+        $clave=mysql_real_escape_string($_POST['Contraseña1']);
+        //variable Number
+        $ci=$_POST['Cedula'];
+        $telf_local=mysql_real_escape_string($_POST['THabitacion']);
+        $telf_cel=mysql_real_escape_string($_POST['TCelular']);
+        
+        //Variables Date
+        $nacimieno=$_POST['Fnacimiento'];
+        $regist=Date("Y-j-m");
+        $direccion=$estado." ".$ciudad." ".$sector;
+        $consul_email="SELECT * from usua WHERE email_usa='".$email."' ";
+        $resltado_email=mysql_query($consul_email);
+        $totalFilas=mysql_num_rows($resltado_email);  
+        if ($totalFilas==1)
+        {
+        	echo "<script type='text/javascript'>alert('AVISO: Error ya existe el E-mail');</script>";
+        }
+        else
+        {
+        	$consul_ci="SELECT * from usua WHERE id _ci='".$ci."' ";
+        	$resltado_ci=mysql_query($consul_ci);
+        	$totalFilas_ci=mysql_num_rows($resltado_ci); 
+        	if ($totalFilas_ci==1) 
+        	{
+        		echo "<script type='text/javascript'>alert('AVISO: Error ya existe la cedula proporcionada');</script>";
+        	}
+        	else
+        	{
+        		mysql_query("INSERT INTO usua
+                   (id_ci,
+                    nom_usa,
+                    ape_usa,
+                    tel_hab_usa,
+                    tel_cel_usa,
+                    fecha_reg_usa,
+                    fecha_nac_usa,
+                    email_usa,
+                    pass_usa,
+                    direc_usa,
+                    desp_usa)
+                    VALUES ('".$ci."',
+                    		'".$nombre."',
+                    		'".$apellido."',
+                    		'".$telf_local."',
+                    		'".$telf_cel."',
+                    		'".$regist."',
+                    		'".$nacimieno."',
+                    		'".$email."',
+                    		'".$clave."',
+                    		'".$direccion."',NULL)",$link);
+             
+	        	$my_error = mysql_error($link);
+
+		        if ($my_error) 
+		        {
+		             echo "<script type='text/javascript'>alert('AVISO: Error en la insercion de datos');</script>";
+		        }else{
+                 
+                 session_start();
+                  $_SESSION["cedula"]= $ci; 
+                  $_SESSION["Nombre_usa"]= $nombre;
+                  $_SESSION["apellido_usa"]= $apellido;
+                  $_SESSION["telefono_h"]= $telf_local;
+                  $_SESSION["telefono_c"]= $telf_cel; 
+                  $_SESSION["fecha_registro"]= $regist;
+                  $_SESSION["fecha_nacimi"]= $nacimieno;
+                  $_SESSION["email"]= $email;
+                  $_SESSION["direccion"]= $direccion; 
+                  $_SESSION["pass"]= $clave;
+
+                  echo "<script type='text/javascript'>alert('Registro Exitoso');</script>";
+
+                  ?><script type="text/javascript">
+                  window.location="index_usa.php";
+                  </script><?php 
+
+            } 
+	    	}  
+        }     
+        mysql_close($link);
+	} 
+}
+?>
+</html>
